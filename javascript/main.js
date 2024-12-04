7d1d1f34987570e9187ca7ace90fcd4d8d51c470
PineappleLord2020 <mul20002@byui.edu>
2024-12-04 09:41:06 -0700

    Fixed the menu staying open on mobile when user is scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,17 @@
 let isSmallScreen = window.innerWidth < 768;
 
 function menuToggle() {
-    const button = document.getElementById('dropDown');
-    const links = document.querySelectorAll('.page_link');
-  
-    if (isSmallScreen) {
+  const button = document.getElementById('dropDown');
+  const links = document.querySelectorAll('.page_link');
+
+  if (isSmallScreen) {
     links.forEach(link => {
       link.style.display = link.style.display === 'block' ? 'none' : 'block';
-      button.style.display = button.style.backgroundColor === "#363731" ? "#c3cf72" : "#363731";
-      button.style.color = button.style.color === "black" ? "white" : "black";
     });
   } else {
     // Always show on larger screens
     links.forEach(link => {
-        link.style.display = 'block';
-        button.style.backgroundColor = "#c3cf72";
-        button.style.color = "black"
+      link.style.display = 'block';
     });
   }
 }
@@ -28,12 +24,13 @@ window.addEventListener('resize', () => {
 document.getElementById("dropDown").addEventListener('click', menuToggle);
 
 var prevScrollpos = window.scrollY;
-window.onscroll = function() {
-  var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav_bar").style.top = "0";
-  } else {
-    document.querySelector(".nav_bar").style.top = "-50px";
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+  const links = document.querySelectorAll('.page_link');
+  const isOpen = links[0].style.display === 'block';
+
+  if (isOpen && isSmallScreen) {
+    menuToggle();
   }
-  prevScrollpos = currentScrollPos;
 }
